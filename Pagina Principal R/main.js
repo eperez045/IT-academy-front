8ca140558b02dce8895957172a1bf73b2a89e7e5
elena <eperez045@gmail.com>
2020-05-18 12:29:33 +0200

    Cambios Pagina Principal
    Cambios antes de correcion pagina principal

--- Pagina Principal R/main.js
+++
@@ -1,9 +0,0 @@
-const selectElement = (s) => document.querySelector(s);
-// Abrir menu por click
-selectElement('.open').addEventListener('click', () => {
-    selectElement('.nav-list').classList.add('active');
-});
-// Cerrar menu por click
-selectElement('.close').addEventListener('click', () => {
-    selectElement('.nav-list').classList.remove('active');
-});
